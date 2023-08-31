41e9ecd023196673abbef75fbec318e515c993f2
Milaweak <milaweak@gmail.com>
2023-08-31 21:10:23 +0200

    starting

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 flex items-center justify-between p-4">
+      <div className="flex items-center text-white mr-6 lg:mr-72">
+        {/* mettre logo ici */}
+        <span className="text-xl font-semibold">GameInc</span>
+      </div>
+      <div className="block lg:hidden">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+        >
+          <svg
+            className={`fill-current h-4 w-4 ${isOpen ? "hidden" : "block"}`}
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+          <svg
+            className={`fill-current h-4 w-4 ${isOpen ? "block" : "hidden"}`}
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+          </svg>
+        </button>
+      </div>
+      <div
+        className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${
+          isOpen ? "block" : "hidden"
+        }`}
+      >
+        <div className="text-sm lg:flex-grow">
+          <a
+            href="#"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white-200 mr-4"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white-200 mr-4"
+          >
+            Second Link
+          </a>
+          <a
+            href="#"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white-200 mr-4"
+          >
+            Third Link
+          </a>
+          <a
+            href="#"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white-200 mr-4"
+          >
+            Profil
+          </a>
+        </div>
+        <div>
+          <button className="inline-flex items-center bg-amber-500 border-0 py-2 px-4 text-white hover:bg-amber-600 rounded-md transition duration-300">
+            Mes jeux
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
